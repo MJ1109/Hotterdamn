a7944d69e4101b8a42218a368b10b90faf647aaf
MJ1109 <manuja115@gmail.com>
2024-02-21 16:18:11 +0100

    added a save tab

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ListScreen from './ListScreen';
 import MapScreen from './MapScreen';
 import SettingsScreen from './SettingsScreen';
+import SavedScreen from './SavedScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +14,7 @@ const Navigation = () => {
     <Tab.Navigator>
       <Tab.Screen name="List" component={ListScreen} />
       <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Saved" component={SavedScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
